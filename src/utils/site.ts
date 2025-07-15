e8871bbae415449fe526b3c451de29398b46753f
tifjaflixe <tifjaflixe@gmail.com>
2025-07-15 14:40:23 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Ngentot Cuttari';
-export const desk = 'Video Bokep Indo Jepang Jav Hijab Live Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
-export const url = 'https://ngentotcuttari.pages.dev';
-export const terbit = '2025-05-16T00:01:03Z';
